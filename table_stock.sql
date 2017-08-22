5aa54369122f5fe8d64b0f0f3f191847a9a7fa88
剑平天下1234 <jptx1234@163.com>
2017-08-22 12:49:28 +0800

    修改库存表：增加库存类型字段

---
+++ table_stock.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.16 : Database - laobeidb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`laobeidb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `laobeidb`;
+
+/*Table structure for table `stock` */
+
+DROP TABLE IF EXISTS `stock`;
+
+CREATE TABLE `stock` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL COMMENT '名称',
+  `unit` varchar(10) DEFAULT NULL COMMENT '单位',
+  `unit_price` float DEFAULT NULL COMMENT '单价',
+  `update_time` datetime DEFAULT NULL COMMENT '更新日期',
+  `stock_type` varchar(20) DEFAULT NULL COMMENT '库存类型',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `stock` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
